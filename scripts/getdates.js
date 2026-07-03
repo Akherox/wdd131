0c3617a9b78b22ccdada3fb97a8380856eb3ad33
alex <bryanalexbm@gmail.com>
2026-07-02 23:10:34 -0300

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Display the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display the date the document was last modified
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
